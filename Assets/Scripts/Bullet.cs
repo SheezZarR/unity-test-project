63d7022f462e7192898491a8f48a128ae32634a6
Iaroslav Baslin <wertout77@gmail.com>
2022-08-21 19:44:49 +0300

    Minor update: simple enemy prefab

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,8 +7,8 @@ public class Bullet : MonoBehaviour
     public GameObject impactEffect;
 
     [SerializeField] private float speed = 20f;
+    [SerializeField] private int damage = 20;
     [SerializeField] private Rigidbody2D rb;
-    
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +16,17 @@ public class Bullet : MonoBehaviour
         rb.velocity = transform.right * speed;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        Enemy enemy = other.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage); 
+
+        }
         GameObject impctEffect = Instantiate(impactEffect, transform.position, transform.rotation);
         Destroy(gameObject);
         Destroy(impctEffect, 0.6f);
-        
     }
 
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int health = 100;
+    private bool is_facingRight = false;
+    private bool is_hit = false;
+    private float dirX = 0.0f;
+
+    private Animator animator;
+    private Rigidbody2D rb;
+    private Transform tr;
+
+    public GameObject deathEffect;
+
+    private enum EnemyState {idle, running, hit};
+    private EnemyState state = EnemyState.idle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Some wandering mechanic
+        dirX = -rb.velocity.x;
+        //
+        UpdateAnimationState();
+
+    }
+
+    private void UpdateAnimationState()
+    {
+
+        if (is_hit)
+        {
+            state = EnemyState.hit;
+        } 
+        else if (dirX > 0f)
+        {
+            state = EnemyState.running;
+            if (!is_facingRight)
+            {
+                Flip();
+            }
+
+        }
+        else if (dirX < 0f)
+        {
+            state = EnemyState.running;
+            if (is_facingRight)
+            {
+                Flip();
+            }
+        }
+        else
+        {
+            state = EnemyState.idle;
+        }
+
+        animator.SetInteger("state", (int)state);
+    }
+    private void Flip()
+    {
+        is_facingRight = !is_facingRight;
+        tr.Rotate(0, 180f, 0);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        is_hit = true;
+        UpdateAnimationState();
+        
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void DisableHitState()
+    {
+        is_hit = false;
+    }
+
+    void Die()
+    {
+        GameObject dethEffect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        Destroy(dethEffect, 0.7f);
+    }
+}
